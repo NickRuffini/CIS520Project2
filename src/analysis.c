331a1045c0fbe904cc34d64f3c454a853f48cfd3
NickRuffini <ruffini@ksu.edu>
2022-03-12 09:08:06 -0600

    Worked on Analysis file; not done yet

--- src/analysis.c
+++ src/analysis.c
@@ -19,7 +19,91 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-    abort();  // replace me with implementation.
+    /*
+
+    //1. Load process control blocks from binary file passed at the command line into a dyn_array (this is your ready queue).
+    dyn_array_t* ready_queue = load_process_control_blocks(argv[1]);
+    
+    //2. Execute your scheduling algorithm to collect the statistics
+    //4. Report your times to STDOUT (copy these to the readme.md file)
+    //IMPORTANT: Will need to change the path of the readme file to your own!
+    FILE* fp;
+    fp = fopen("/homes/ruffini/CIS520Project2/readme.md", "w");
+    
+    ScheduleResult_t scheduleResult;
+    
+    // Check to see what algorithm we are using, and act accordingly
+    if(strcmp("FCFS", argv[2]) == 0) {
+        // prints to our readme.md file!
+        fprintf(fp, "First Come First Serve \n");
+        
+        bool result = first_come_first_serve(ready_queue, &scheduleResult);
+        // if the algorithm fails for whatever reason
+        if (result == false) {
+            fprintf(fp, "***Algorithm failed to complete*** \n");
+            fclose(fp);
+            free(ready_queue);
+            return EXIT_FAILURE;
+        }
+    }
+    else if(strcmp("P", argv[2]) == 0) {
+        fprintf(fp, "Priority Scheduling \n");
+    }
+    else if(strcmp("RR", argv[2]) == 0) {
+        fprintf(fp, "Round Robin: \n");
+        
+        bool result = round_robin(ready_queue, &scheduleResult, (size_t)atoi(argv[3]));
+        // if the algorithm fails for whatever reason
+        if (result == false) {
+            fprintf(fp, "***Algorithm failed to complete*** \n");
+            fclose(fp);
+            free(ready_queue);
+            return EXIT_FAILURE;
+        }
+    }
+    else if(strcmp("SJF", argv[2]) == 0) {
+        fprintf(fp, "Shortest Job First \n");
+        
+        bool result = shortest_job_first(ready_queue, &scheduleResult);
+        // if the algorithm fails for whatever reason
+        if (result == false) {
+            fprintf(fp, "***Algorithm failed to complete*** \n");
+            fclose(fp);
+            free(ready_queue);
+            return EXIT_FAILURE;
+        }
+    }
+    else if(strcmp("SRT", argv[2]) == 0) {
+        fprintf(fp, "Shortest Remaining Time \n");
+        
+        bool result = shortest_remaining_time_first(ready_queue, &scheduleResult);
+        // if the algorithm fails for whatever reason
+        if (result == false) {
+            fprintf(fp, "***Algorithm failed to complete*** \n");
+            fclose(fp);
+            free(ready_queue);
+            return EXIT_FAILURE;
+        }
+    }
+    else {
+        printf("Not a valid algorithm type!");
+        return EXIT_FAILURE;
+    }
+    
+    // Now, just print the 
+    // 1. float Average Waiting Time
+    // 2. float Average Turnaround Time
+    // 3. unsigned long Total Clock Time
+    // to the readme!
+    
+    fprintf(fp, "Average Waiting Time: %.3f \n", (float)scheduleResult.average_waiting_time);
+    fprintf(fp, "Average Turnaround Time: %.3f \n", (float)scheduleResult.average_turnaround_time);
+    fprintf(fp, "Total Clock Time: %.3f \n", (float)scheduleResult.total_run_time);
+    
+    //3. Clean up any allocations
+    free(ready_queue);
+    
+    */
 
     return EXIT_SUCCESS;
 }
